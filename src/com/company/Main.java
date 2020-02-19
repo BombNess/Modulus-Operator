3f51b3666420d9796a1ee08564b92c4a44e9c245
levi <levi.rappleye.2023@student.lhschools.org>
2020-02-19 13:45:04 -0500

    OddnessMethodCreated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main {
+    public static void main(String[] args){
+        // todo even or odd
+        //todo keep number contained
+        //todo get the last digit
+        //We want to find if a number is even or odd
+        testOddness();
+    }
+
+    public static void testOddness() {
+        Scanner Keyboard;
+        System.out.println("Type a Positive Number");
+        Keyboard = new Scanner(System.in);
+        int number = Keyboard.nextInt();
+        System.out.println("Number is even: " + (number % 2 ==0));
+        System.out.println("Number is odd: " + (number % 2 !=0));
+
+
     }
 }
